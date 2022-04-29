fbda70b2f87c61ca4ff06830e17c905309310ca4
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-29 20:03:22 -0300

    Renderização de lista em componentes

--- 3_avancado/avancado/src/App.js
+++ 3_avancado/avancado/src/App.js
@@ -9,6 +9,12 @@ import './App.css';
 import City from "./assets/city.jpg";
 
 function App() {
+  const cars = [
+    {id: 1, brand: "Ferrari", km: 0, color: "Vermelho", newCar: true},
+    {id: 2, brand: "Kia", km: 34343, color: "Branco", newCar: false},
+    {id: 3, brand: "Renault", km: 234, color: "Azul", newCar: false},
+  ];
+
   return (
     <div className="App">
       <h1>Avançando em React</h1>
@@ -16,19 +22,28 @@ function App() {
       <div>
         <img src="/img1.jpg" alt="Paisagem"/>
       </div>
+
       {/* Imagem em assets */}
       <div>
         <img src={City} alt="Cidade" />
       </div>
+
       <ManageData />
       <ListRender />
       <ConditionalRender />
+
       {/* Props */}
       <ShowUserName name="Alexandre"/>
+
       {/* Destructuring e reaproveitamento*/}
       <CarDetails brand="VW" km={100000} color="Azul" newCar={false}/>
       <CarDetails brand="Ford" km={0} color="Vermelho" newCar={true}/>
       <CarDetails brand="Fiat" km={4500} color="Branco" newCar={false}/>
+
+      {/* loop em array de objetos */}
+      {cars.map((car) => (
+        <CarDetails key={car.id} brand={car.brand} km={car.km} color={car.color} newCar={car.newCar} />
+      ))}
     </div>
   );
 }
