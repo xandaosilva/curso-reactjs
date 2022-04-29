0489ab3137bb3e90fa045705ecf1b8fc91a4c9bf
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-29 16:33:34 -0300

    Criando e importando components

---
+++ 2_fundamentos/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FirstComponent = () => {
+    // Primeiro comentário
+
+    return (
+        <div>
+            {/* Segundo comentário */}
+            <h1>Meu primeiro componente</h1>
+            <p className="teste">Conteúdo do componente</p>
+        </div>
+    )
+}
+
+export default FirstComponent;
