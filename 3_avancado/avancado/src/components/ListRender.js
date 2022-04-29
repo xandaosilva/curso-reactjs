78822104fc9f07a1b6a3e59eb47a05840a3b3003
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-29 18:56:15 -0300

    A propriedade key

--- 3_avancado/avancado/src/components/ListRender.js
+++ 3_avancado/avancado/src/components/ListRender.js
@@ -3,12 +3,22 @@ import React, { useState } from 'react'
 const ListRender = () => {
 
     const [list] = useState(["Eduardo", "Mônica", "Laura"]);
+    const [users] = useState([
+        {id: 1, name:"Eduardo", age: 27 },
+        {id: 2, name:"Monica", age: 25},
+        {id: 3, name:"Laura", age: 32},
+    ]);
 
     return (
         <div>
             <ul>
-                {list.map((item) => (
-                    <li>{item}</li>
+                {list.map((item, i) => (
+                    <li key={i}>{item}</li>
+                ))}
+            </ul>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name} - {user.age}</li>
                 ))}
             </ul>
         </div>
