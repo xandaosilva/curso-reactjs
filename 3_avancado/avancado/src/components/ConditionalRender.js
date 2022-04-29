c853acdc4aaee76ee6408cebc09f22b93d210c9d
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-29 19:23:26 -0300

    Renderização condicional (ternário)

--- 3_avancado/avancado/src/components/ConditionalRender.js
+++ 3_avancado/avancado/src/components/ConditionalRender.js
@@ -2,12 +2,16 @@ import React, { useState } from 'react'
 
 const ConditionalRender = () => {
     const [x] = useState(true);
+    const [name, setName] = useState("Teste");
 
     return (
         <div>
             <h1>Isso será exibido ?</h1>
             {x && <p>Se x for true, sim !</p>}
             {!x && <p>Se x for false, sim !</p>}
+            <h1>If ternário</h1>
+            {name === "João" ? (<div><p>O nome é João</p></div>) : (<div><p>Nome não encontrado</p></div>)}
+            <button onClick={() => setName("João")}>Clique aqui!</button>
         </div>
     )
 }
