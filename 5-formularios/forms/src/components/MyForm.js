fce4183f132fa087395972d729f72813006149d2
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-30 01:43:12 -0300

    Select no react

--- 5-formularios/forms/src/components/MyForm.js
+++ 5-formularios/forms/src/components/MyForm.js
@@ -6,7 +6,8 @@ const MyForm = ({user}) => {
   // Gerenciamento de dados
   const [name, setName] = useState(user ? user.name : "");
   const [email, setEmail] = useState(user ? user.email : "");
-  const [bio, setBio] = useState("");
+  const [bio, setBio] = useState(user ? user.bio : "");
+  const [role, setRole] = useState(user ? user.role : "");
 
   const handleName = (e) => {
     setName(e.target.value);
@@ -15,11 +16,12 @@ const MyForm = ({user}) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Enviando o formulário");
-    console.log(name, email, bio);
+    console.log(name, email, bio, role);
 
     setName("");
     setEmail("");
     setBio("");
+    setRole("");
   }
 
   return (
@@ -41,6 +43,16 @@ const MyForm = ({user}) => {
         <label>
           <span>Bio</span>
           <textarea name="bio" placeholder="Descrição do usuário" onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+        </label>
+
+        {/* Select */}
+        <label>
+          <span>Função no sistema</span>
+          <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+            <option value="user">Usuário</option>
+            <option value="editor">Editor</option>
+            <option value="admin">Administrador</option>
+          </select>
         </label>
 
         <input type="submit" value="Enviar" />
