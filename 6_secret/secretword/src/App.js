ca9c7cae33e971541edcd80e4aa6ec0b0fc557ee
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-04-30 02:42:43 -0300

    Criando os componentes do jogo Secret words

--- 6_secret/secretword/src/App.js
+++ 6_secret/secretword/src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+
 import StartScreen from './components/StartScreen';
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+import { wordsList } from "./data/words";
+
+import './App.css';
+
+const stages = [{ id: 1, name: "start" }, { id: 2, name: "game" }, { id: 3, name: "end" }];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
