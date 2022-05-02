28541ce2e47def64227aa17dd0371ba8d3920e84
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-02 17:32:29 -0300

    Alternando os estágios do jogo

--- 06_secret/secretword/src/components/GameOver.js
+++ 06_secret/secretword/src/components/GameOver.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import "./GameOver.css";
 
-const GameOver = () => {
+const GameOver = ({ retry }) => {
     return (
-        <div>GameOver</div>
+        <div>
+            <h1>GameOver</h1>
+            <button onClick={retry}>Recomeçar o jogo</button>
+        </div>
     );
 }
 
