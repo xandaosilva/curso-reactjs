03e2a96e63b643e63fe7f94ae3c4db4b9b88a943
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-02 18:00:21 -0300

    Escolhendo a palavra e a categoria de maneira aleatória

--- 06_secret/secretword/src/App.js
+++ 06_secret/secretword/src/App.js
@@ -13,8 +13,27 @@ const stages = [{ id: 1, name: "start" }, { id: 2, name: "game" }, { id: 3, name
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return {word, category};
+  }
 
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
     setGameStage(stages[1].name);
   }
 
