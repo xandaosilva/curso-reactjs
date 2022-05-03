e8c5e32c94c31ecbe38714f45902c6f9117c4be0
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-02 23:04:13 -0300

    Resgatando dados da API

--- 07_req_http/httpreact/src/App.js
+++ 07_req_http/httpreact/src/App.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
+const url = "http://localhost:3000/products";
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
