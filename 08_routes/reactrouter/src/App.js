7b86402d1bfa0963c70dfd43d872c1df10e2cb8f
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-03 15:26:35 -0300

    Navegação entre páginas

--- 08_routes/reactrouter/src/App.js
+++ 08_routes/reactrouter/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 
+import Navbar from './components/Navbar';
+
 import './App.css';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
