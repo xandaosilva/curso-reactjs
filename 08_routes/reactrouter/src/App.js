5e6ed8be5b65a0ebae4a4983d54892806154057a
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-03 17:11:49 -0300

    Página 404

--- 08_routes/reactrouter/src/App.js
+++ 08_routes/reactrouter/src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 import Navbar from './components/Navbar';
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
