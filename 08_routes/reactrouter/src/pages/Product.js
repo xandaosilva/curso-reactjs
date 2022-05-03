f6f8d6e17a2b6ed27a9bad567f710ee695c3c3ed
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-03 16:21:13 -0300

    Carregamento dinâmico de dados

--- 08_routes/reactrouter/src/pages/Product.js
+++ 08_routes/reactrouter/src/pages/Product.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import { useParams } from 'react-router-dom';
+import { useFetch } from '../hooks/useFetch';
 
 const Product = () => {
   const { id } = useParams();
+  const url = `http://localhost:3000/products/${id}`;
+  const { data: product, loading, error } = useFetch(url); 
 
   return (
     <>
       <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro ...</p>}
+      {loading && <p>Carregando ...</p>}
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R${product.price}</p>
+        </div>
+      )}
     </>
   )
 }
