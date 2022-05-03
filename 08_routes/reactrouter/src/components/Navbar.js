ec38a6449629ab00c5ad0439ad35c5ca8871c3a6
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-03 17:23:03 -0300

    LInks ativos

--- 08_routes/reactrouter/src/components/Navbar.js
+++ 08_routes/reactrouter/src/components/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import "./Navbar.css";
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            {/* <Link to="/">Home</Link>
+            <Link to="/about">About</Link> 
+            <NavLink to="/" className={({ isActive }) => isActive ? "esta_ativo" : "nao_esta_ativo"}>Home</NavLink>*/}
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
         </nav>
     );
 }
