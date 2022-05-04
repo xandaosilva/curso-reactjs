b15586c00fabddea5baef6711b45e42e0b5434e7
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-04 01:39:17 -0300

    Alterando contexto mais complexo

--- 09_context/contextapi/src/pages/Products.js
+++ 09_context/contextapi/src/pages/Products.js
@@ -2,14 +2,16 @@
 // import { CounterContext } from '../context/CounterContext';
 
 import { useCounterContext } from '../hooks/useCounterContext';
+import { useTitleColorContext } from '../hooks/useTitleColorContext';
 
 const Products = () => {
   // const { counter } = useContext(CounterContext);
   const { counter } = useCounterContext();
+  const { color } = useTitleColorContext();
 
   return (
     <div>
-      <h1>Products</h1>
+      <h1 style={{color: color}}>Products</h1>
       <p>Valor do contador: {counter}</p>
     </div>
   )
