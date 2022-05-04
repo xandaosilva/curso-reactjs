871cc516ab3835a52d368774aafa98a52dba890d
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-03 21:25:46 -0300

    Redirecionamento de URL

--- 08_routes/reactrouter/src/App.js
+++ 08_routes/reactrouter/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -25,6 +25,7 @@ function App() {
           <Route path="/products/:id/info" element={<Info />} />
           <Route path="/products/:id" element={<Product />} />
           <Route path="/search" element={<Search />} />
+          <Route path="company" element={<Navigate to="/about" />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
