2e0f35b2620fa0db09a0f415ce470a25bf85076e
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-04 01:20:50 -0300

    Criando um contexto mais complexo

--- 09_context/contextapi/src/pages/Home.js
+++ 09_context/contextapi/src/pages/Home.js
@@ -6,14 +6,16 @@ import ChangeCounter from '../components/ChangeCounter';
 // import { CounterContext } from '../context/CounterContext';
 
 import { useCounterContext } from '../hooks/useCounterContext';
+import { useTitleColorContext } from '../hooks/useTitleColorContext';
 
 const Home = () => {
   // const { counter } = useContext(CounterContext);
   const { counter } = useCounterContext();
+  const { color } = useTitleColorContext();
 
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{color: color}}>Home</h1>
       <p>Valor do contador: {counter}</p>
       <ChangeCounter />
     </div>
