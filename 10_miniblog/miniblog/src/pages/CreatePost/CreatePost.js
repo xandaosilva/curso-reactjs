67ec1a519066cef7d6c8d5da72fa3b8ecff249a4
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 11:17:58 -0300

    Inserindo dados no firebase

--- 10_miniblog/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_miniblog/miniblog/src/pages/CreatePost/CreatePost.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { useAuthValue } from "../../context/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 
 import styles from "./CreatePost.module.css";
 
@@ -11,9 +12,15 @@ const CreatePost = () => {
     const [body, setBody] = useState("");
     const [tags, setTags] = useState("");
     const [formError, setFormError] = useState([]);
+    const {insertDocument, response} = useInsertDocument("posts");
+    const {user} = useAuthValue();
+
 
     const handleSubmit = (e) =>{
         e.preventDefault();
+        setFormError("");
+
+        insertDocument({ title, image, body, tags, uid: user.uid, createdBy: user.displayName });
     }
     
     return (
@@ -21,26 +28,25 @@ const CreatePost = () => {
             <h2>Criar Post</h2>
             <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="">
+                <label>
                     <span>Título:</span>
                     <input type="text" name="title" required placeholder="Pense em um bom título..." onChange={(e) => setTitle(e.target.value)} value={title} />
                 </label>
-                <label htmlFor="">
+                <label>
                     <span>URL da imagem:</span>
                     <input type="text" name="image" required placeholder="Insira uma imagem que represente o seu post" onChange={(e) => setImage(e.target.value)} value={image} />
                 </label>
-                <label htmlFor="">
+                <label>
                     <span>Conteúdo:</span>
                     <textarea name="body" required placeholder="Insira o conteúdo do post" onChange={(e) => setBody(e.target.value)} value={body}></textarea>
                 </label>
-                <label htmlFor="">
+                <label>
                     <span>Tags:</span>
                     <input type="text" name="tags" required placeholder="Insira as tagas separadas por vírgula" onChange={(e) => setTags(e.target.value)} value={tags} />
                 </label>
-                <button className="btn">Cadastrar</button>
-                {/* {!loading && <button className="btn">Cadastrar</button>}
-                {loading && <button className="btn" disabled>Aguarde...</button>}
-                {error && <p className="error">{error}</p>} */}
+                {!response.loading && <button className="btn">Cadastrar</button>}
+                {response.loading && <button className="btn" disabled>Aguarde...</button>}
+                {response.error && <p className="error">{response.error}</p>}
             </form>
         </div>
     )
