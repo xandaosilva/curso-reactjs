fec66fcd6d3930a21abc3b4ac972d72ce8a05a42
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 17:58:35 -0300

    Estruturando a home

--- 10_miniblog/miniblog/src/pages/Home/Home.js
+++ 10_miniblog/miniblog/src/pages/Home/Home.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
 import styles from "./Home.module.css";
 
 const Home = () => {
+    const [query, setQuery] = useState("");
+    const [posts] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.home}>
+            <h1>Veja os nossos posts mais recentes</h1>
+            <form onSubmit={handleSubmit} className={styles.search_form}>
+                <input type="text" placeholder="Ou busque por tags..." onChange={(e) => setQuery(e.target.value)} />
+                <button className="btn btn-dark">Pesquisar</button>
+            </form>
+            <div>
+                <h1>Posts...</h1>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>Não foram criados posts</p>
+                        <Link to="/posts/create" className="btn">Criar primeiro post</Link>
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
