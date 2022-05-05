9f1e38f007363d3a31eda02a015df10b767fcc7b
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 10:18:09 -0300

    Estrutura da página de criar post

--- 10_miniblog/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_miniblog/miniblog/src/pages/CreatePost/CreatePost.js
@@ -1,8 +1,47 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAuthValue } from "../../context/AuthContext";
+
 import styles from "./CreatePost.module.css";
+
 const CreatePost = () => {
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [body, setBody] = useState("");
+    const [tags, setTags] = useState("");
+    const [formError, setFormError] = useState([]);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+    
     return (
-        <div>
-            <h1>CreatePost</h1>
+        <div className={styles.create_post}>
+            <h2>Criar Post</h2>
+            <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="">
+                    <span>Título:</span>
+                    <input type="text" name="title" required placeholder="Pense em um bom título..." onChange={(e) => setTitle(e.target.value)} value={title} />
+                </label>
+                <label htmlFor="">
+                    <span>URL da imagem:</span>
+                    <input type="text" name="image" required placeholder="Insira uma imagem que represente o seu post" onChange={(e) => setImage(e.target.value)} value={image} />
+                </label>
+                <label htmlFor="">
+                    <span>Conteúdo:</span>
+                    <textarea name="body" required placeholder="Insira o conteúdo do post" onChange={(e) => setBody(e.target.value)} value={body}></textarea>
+                </label>
+                <label htmlFor="">
+                    <span>Tags:</span>
+                    <input type="text" name="tags" required placeholder="Insira as tagas separadas por vírgula" onChange={(e) => setTags(e.target.value)} value={tags} />
+                </label>
+                <button className="btn">Cadastrar</button>
+                {/* {!loading && <button className="btn">Cadastrar</button>}
+                {loading && <button className="btn" disabled>Aguarde...</button>}
+                {error && <p className="error">{error}</p>} */}
+            </form>
         </div>
     )
 }
