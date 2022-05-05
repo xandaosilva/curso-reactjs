0cb57a0d2dcdf6969fd043cf94526a078b426a39
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 08:46:55 -0300

    Links para usuários autenticados

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -6,6 +6,8 @@ import About from "./pages/About/About";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -43,6 +45,8 @@ function App() {
               <Route path="/about" element={<About />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/posts/create" element={<CreatePost />} />
+              <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
           </div>
           <Footer />
--- 10_miniblog/miniblog/src/components/Navbar.js
+++ 10_miniblog/miniblog/src/components/Navbar.js
@@ -1,8 +1,13 @@
 import { NavLink } from "react-router-dom";
 
+import { useAuthentication } from "../hooks/useAuthentication";
+import { useAuthValue } from "../context/AuthContext";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+    const { user } = useAuthValue();
+
     return (
         <nav className={styles.navbar}>
             <NavLink to="/" className={styles.brand}>Mini <span>Blog</span></NavLink>
@@ -10,12 +15,26 @@ const Navbar = () => {
                 <li>
                     <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>Home</NavLink>
                 </li>
-                <li>
-                    <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Login</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
-                </li>
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar</NavLink>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({isActive}) => (isActive ? styles.active : "")}>Criar post</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/dashboard" className={({isActive}) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+                        </li>
+                    </>
+                )}
                 <li>
                     <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink>
                 </li>
