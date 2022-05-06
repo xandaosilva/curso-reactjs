20089b873646d844ff4b412706fc79eb2cd75f45
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 23:23:17 -0300

    CSS da página de post individual

--- 10_miniblog/miniblog/src/pages/Post/Post.js
+++ 10_miniblog/miniblog/src/pages/Post/Post.js
@@ -8,11 +8,19 @@ const Post = () => {
     const { document: post, loading } = useFetchDocument("posts", id);
 
     return (
-        <div>
+        <div className={styles.post_container}>
             {loading && <p>Carregando post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>Este post trata sobre:</h3>
+                    <div className={styles.tags}>
+                        {post.tags.map((tag) => (
+                            <p key={tag}><span>#</span>{tag}</p>
+                        ))}
+                    </div>
                 </>
             )}
         </div>
