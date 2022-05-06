a0c6ea1db6ce360eba8d79898bfc420ce25cff5d
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-05 21:55:02 -0300

    Criando funcionalidade de busca

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -8,6 +8,7 @@ import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Search from "./pages/Seacrh/Search";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -43,6 +44,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/search" element={<Search />} />
               <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
               <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
               <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/login" />} />
--- 10_miniblog/miniblog/src/pages/Home/Home.js
+++ 10_miniblog/miniblog/src/pages/Home/Home.js
@@ -9,9 +9,14 @@ import styles from "./Home.module.css";
 const Home = () => {
     const [query, setQuery] = useState("");
     const {documents: posts, loading} = useFetchDocuments("posts");
+    const navigate = useNavigate();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        if(query){
+            return navigate(`/search?q=${query}`);
+        }
     }
 
     return (
