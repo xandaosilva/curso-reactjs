0c8dd1910f01016439c3ab42d2a36aff2c489665
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-06 01:43:01 -0300

    Estruturando o dashboard

--- 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
+
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
+    const { user } = useAuthValue();
+    const uid = user.uid;
+    const posts = [];
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h2>Dashboard</h2>
+            <p>Gerencie os seus posts</p>
+            {posts && posts.length === 0 ? 
+                (<div className={styles.noposts}>
+                    <p>Não foram encontrados posts</p>
+                    <Link to="/posts/create" className="btn">Criar o primeiro post</Link>
+                </div>) : 
+                (<div>
+                    <p>Tem posts</p>
+                </div>)
+            }
         </div>
     )
 }
