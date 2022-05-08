1ad0ef1f9555c87f25d7eeb33fb2bde1c2ad5ebf
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-08 05:03:30 -0300

    useState e inputs

--- 11_hooks/reacthooks/src/components/HookUseState.js
+++ 11_hooks/reacthooks/src/components/HookUseState.js
@@ -3,10 +3,16 @@ import { useState } from "react";
 const HookUseState = () => {
     let userName = "João";
     const [name, setName] = useState("Maria");
+    const [age, setAge] = useState(18);
 
     const changeNames = () => {
         userName = "João da Silva";
         setName("Maria Eduarda");
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(age);
     }
 
     return (
@@ -15,6 +21,15 @@ const HookUseState = () => {
             <p>Variável: {userName}</p>
             <p>useState: {name}</p>
             <button onClick={changeNames}>Alterar nomes</button>
+            <hr />
+            
+            <h2>useState e inputs</h2>
+            <p>Digite a sua idade</p>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                <input type="submit" value="Enviar" />
+            </form>
+            <p>Você tem {age} anos</p>
             <hr />
         </div>
     );
