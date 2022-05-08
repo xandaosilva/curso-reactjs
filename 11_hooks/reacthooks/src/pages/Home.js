1841c9e56860337063c0f0d7e1369736b8e78d1e
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-08 08:21:59 -0300

    useCallback

--- 11_hooks/reacthooks/src/pages/Home.js
+++ 11_hooks/reacthooks/src/pages/Home.js
@@ -4,6 +4,7 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallback from "../components/HookUseCallback";
 
 import { SomeContext } from "../components/HookUseContext";
 
@@ -19,6 +20,7 @@ const Home = () => {
             <p>Valor do contexto: {contextValue}</p>
             <hr />
             <HookUseRef />
+            <HookUseCallback />
         </div>
     );
 }
