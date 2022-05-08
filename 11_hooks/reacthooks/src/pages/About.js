f66848c5a912d908cacdec31a5465f7608410f46
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-08 06:56:57 -0300

    useContext

--- 11_hooks/reacthooks/src/pages/About.js
+++ 11_hooks/reacthooks/src/pages/About.js
@@ -1,6 +1,16 @@
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
+
 const About = () => {
+    const { contextValue } = useContext(SomeContext);
+
     return (
-        <div>About</div>
+        <div>
+            <h2>About</h2>
+            <h2>useContext</h2>
+            <p>Valor do contexto: {contextValue}</p>
+            <hr />
+        </div>
     );
 }
 
--- 11_hooks/reacthooks/src/pages/Home.js
+++ 11_hooks/reacthooks/src/pages/Home.js
@@ -1,13 +1,22 @@
+import { useContext } from "react";
+
 import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 
+import { SomeContext } from "../components/HookUseContext";
+
 const Home = () => {
+    const { contextValue } = useContext(SomeContext);
+
     return (
         <div>
             <HookUseState />
             <HookUseReducer />
             <HookUseEffect />
+            <h2>useContext</h2>
+            <p>Valor do contexto: {contextValue}</p>
+            <hr />
         </div>
     );
 }
