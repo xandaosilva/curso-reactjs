aeebc1b0eb1f4c41694a65c2a86340778b3bb9d5
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-09 18:27:23 -0300

    Conexão com o banco de dados

--- 12_reactgram/backend/config/db.js
+++ 12_reactgram/backend/config/db.js
@@ -1,1 +1,18 @@
 const mongoose = require("mongoose");
+
+// conection
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async() => {
+    try {
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.dcdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+        console.log("Conectou ao banco de dados");
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+conn();
+module.exports = conn();
