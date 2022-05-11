0bee2e05e7bb36be36912460d008716384dea1ec
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-10 23:05:11 -0300

    Registro do usuário

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
 const jwtSecret = process.env.JWT_SECRET;
 
 // Generate user token
@@ -10,7 +11,28 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async(req, res) => {
-    res.send("Registro");
+    const { name, email, password } = req.body;
+    const user = await User.findOne({email});
+
+    if(user){
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail."]});
+        return;
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt();
+    const passwordhash = await bcrypt.hash(password, salt);
+
+    // Create user
+    const newUser = await User.create({ name, email, password: passwordhash });
+
+    // If user was created successfully, return the token
+    if(!newUser){
+        res.status(422).json({ errors: ["Houve um erro, por favor tente mais tarde."] });
+        return;
+    }
+
+    res.status(201).json({ _id: newUser._id, token: generateToken(newUser._id) });
 }
 
 module.exports = { register };
