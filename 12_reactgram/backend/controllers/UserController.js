155cdb63fa93346d9455a25a94ee5bad43d5db60
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-24 15:59:03 -0300

    Resgatando usuário pelo id

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -94,4 +94,23 @@ const update = async (req, res) => {
     res.status(200).json(user);
 }
 
-module.exports = { register, login, getCurrentUser, update };
+// Get user by id
+const getUserById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password");
+        
+        if(!user){
+            res.status(404).json({errors: ["Usuário não encontrado."]});
+            return;
+        }
+    
+        res.status(200).json(user); 
+    } catch (error) {
+        res.status(404).json({errors: ["Usuário não encontrado."]});
+        return;
+    }
+}
+
+module.exports = { register, login, getCurrentUser, update, getUserById };
