17b23f908361208635891d75fa2aac290a652659
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-23 21:14:31 -0300

    Middleware de upload de imagens

---
+++ 12_reactgram/backend/middlewares/imageUpload.js
@@ -0,0 +1,32 @@
+const multer = requier("multer");
+const path = require("path");
+
+// Destination to storage image
+const imageStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        let folder = "";
+
+        if(req.baseUrl.includes("users"))
+            folder = "users";
+        else if(req.baseUrl.includes("photos"))
+            folder = "photos";
+
+        cb(null, `uploads/${folder}/`);
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            // upload only png and jpg image
+            return cb(new Error("Por favor, envie apenas png ou jpg"));
+        }
+        cb(undefined, true);
+    }
+});
+
+module.exports = { imageUpload };
