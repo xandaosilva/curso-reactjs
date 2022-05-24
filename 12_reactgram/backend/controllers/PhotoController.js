a9415d77b34c8ba3deab20c6162510eff2df16b8
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-24 18:12:38 -0300

    Resgatando todas as fotos

--- 12_reactgram/backend/controllers/PhotoController.js
+++ 12_reactgram/backend/controllers/PhotoController.js
@@ -49,4 +49,10 @@ const deletePhoto = async (req, res) => {
     }
 }
 
-module.exports = { insertPhoto, deletePhoto };
+// Get all photos
+const getAllPhotos = async (req, res) => {
+    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec();
+    return res.status(200).json(photos);
+}
+
+module.exports = { insertPhoto, deletePhoto, getAllPhotos };
