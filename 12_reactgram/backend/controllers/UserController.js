f95538665c26cd2ddf0c43fd110a41f05c3cc87f
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-24 15:39:55 -0300

    Atualizando usuário

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -64,7 +64,34 @@ const getCurrentUser = async (req, res) => {
 
 // Update an user
 const update = async (req, res) => {
-    res.send("Update");
+    const { name, password, bio } = req.body;
+    let profileImage = null;
+
+    if(req.file)
+        profileImage = req.file.filename;
+
+    const reqUser = req.user;
+    const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password");
+
+    if(name)
+        user.name = name;
+
+    if(password){
+        // Generate password hash
+        const salt = await bcrypt.genSalt();
+        const passwordhash = await bcrypt.hash(password, salt);
+        user.password = passwordhash;
+    }
+
+    if(profileImage)
+        user.profileImage = profileImage;
+
+    if(bio)
+        user.bio = bio;
+
+    await user.save();
+
+    res.status(200).json(user);
 }
 
 module.exports = { register, login, getCurrentUser, update };
