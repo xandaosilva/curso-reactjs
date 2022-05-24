bb7cd38a72c9b0383220f2e723af484344724fcd
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-24 17:59:59 -0300

    Excluíndo fotos

--- 12_reactgram/backend/controllers/PhotoController.js
+++ 12_reactgram/backend/controllers/PhotoController.js
@@ -14,10 +14,39 @@ const insertPhoto = async (req, res) => {
 
     // If photo was created successfuly, return data
     if(!newPhoto){
-        res.status(422).json({errors: ["Houve um problema, pro favor tente novamente mais tarde."]});
+        res.status(422).json({errors: ["Houve um problema, por favor tente novamente mais tarde."]});
+        return;
     }
 
     res.status(201).json(newPhoto);
 }
 
-module.exports = { insertPhoto };
+// Remove a photo from DB
+const deletePhoto = async (req, res) => {
+    const { id } = req.params;
+    const reqUser = req.user;
+    
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id));
+
+        // Check if photo exists
+        if(!photo){
+            res.status(404).json({errors: ["Foto não encontrada."]});
+            return;
+        }
+
+        // Check if photo belongs to user
+        if(!photo.userId.equals(reqUser._id)){
+            res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde."]});
+            return;
+        }
+
+        await Photo.findByIdAndDelete(photo._id);
+        res.status(200).json({id: photo._id, message: "Foto excluída com sucesso."});
+    } catch (error) {
+        res.status(404).json({errors: ["Foto não encontrada."]});
+        return;
+    }
+}
+
+module.exports = { insertPhoto, deletePhoto };
