b9653f8f91e93662466db6e6c679a1d008c48041
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-26 18:08:09 -0300

    Conectando formulário ao reducer

--- 12_reactgram/frontend/src/pages/Auth/Register.js
+++ 12_reactgram/frontend/src/pages/Auth/Register.js
@@ -6,11 +6,19 @@ import { Link } from "react-router-dom";
 // Hooks
 import { useState, useEffect } from "react";
 
+// Redux
+import { register, reset } from "../../slices/authSlice";
+import { useSelector, useDispatch } from "react-redux";
+
 const Register = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+
+    const dispatch = useDispatch();
+
+    const { loading, error } = useSelector((state) => state.auth);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -18,7 +26,13 @@ const Register = () => {
         const user = { name, email, password, confirmPassword };
 
         console.log(user);
+        dispatch(register(user));
     }
+
+    // Clean all auth states
+    useEffect(() => {
+        dispatch(reset());
+    }, [dispatch]);
 
     return (
         <div id="register">
