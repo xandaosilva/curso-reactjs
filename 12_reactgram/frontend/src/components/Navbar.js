86d0209a2ad192cda6acbfb721fda955a3a82377
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-26 20:15:31 -0300

    Navbar para usuários autenticados

--- 12_reactgram/frontend/src/components/Navbar.js
+++ 12_reactgram/frontend/src/components/Navbar.js
@@ -4,7 +4,16 @@ import "./Navbar.css";
 import { NavLink, Link } from "react-router-dom";
 import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from "react-icons/bs";
 
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const { auth } = useAuth();
+    const { user } = useSelector((state) => state.auth);
+
     return (
         <nav id="nav">
             <Link to="/">ReactGram</Link>
@@ -13,17 +22,39 @@ const Navbar = () => {
                 <input type="text" placeholder="Pesquisar"/>
             </form>
             <ul id="nav-links">
-                <li>
-                    <NavLink to="/">
-                        <BsHouseDoorFill />
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">Entrar</NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register">Cadastrar</NavLink>
-                </li>
+                {auth ? (
+                    <>
+                        <li>
+                            <NavLink to="/">
+                                <BsHouseDoorFill />
+                            </NavLink>
+                        </li>
+                        {user && (
+                            <li>
+                                <NavLink to={`/users/${user._id}`}>
+                                    <BsFillCameraFill />
+                                </NavLink>
+                            </li>
+                        )}
+                        <li>
+                            <NavLink to="/profile">
+                                <BsFillPersonFill />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <span>Sair</span>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li>
+                            <NavLink to="/login">Entrar</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register">Cadastrar</NavLink>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     );
