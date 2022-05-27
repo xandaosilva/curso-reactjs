ff2193defc4805e2b4cab8013dff5eec3859e02d
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 13:54:16 -0300

    Service de atualização de usuário

--- 12_reactgram/frontend/src/services/userService.js
+++ 12_reactgram/frontend/src/services/userService.js
@@ -13,5 +13,18 @@ const profile = async (data, token) => {
     }
 }
 
-const userService = { profile };
+// Update user details
+const updateProfile = async (data, token) => {
+    const config = requestConfig("PUT", data, token, true);
+
+    try {
+        const res = await fetch(api + "/users/", config).then((res) => res.json()).catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const userService = { profile, updateProfile };
 export default userService;
