1d53bb09b4bf3917893a03b9317a60c6642813ec
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 19:22:29 -0300

    Slice de inserção de foto

--- 12_reactgram/frontend/src/slices/photoSlice.js
+++ 12_reactgram/frontend/src/slices/photoSlice.js
@@ -3,8 +3,18 @@ import photoService from "../services/photoService";
 
 const initialState = { photos: [], photo: {}, error: false, success: false, loading: false, message: null };
 
-// Functions
+// Publish user photo
+export const publishPhoto = createAsyncThunk("photo/publish", async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.publishPhoto(photo, token);
 
+    // Check for errors
+    if(data.errors){
+        return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+});
 
 export const photoSlice = createSlice({
     name: "photo",
@@ -13,6 +23,23 @@ export const photoSlice = createSlice({
         resetMessage: (state) => {
             state.message = null;
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(publishPhoto.pending, (state) => {
+            state.loading = true;
+            state.error = false;
+        }).addCase(publishPhoto.fulfilled, (state, action) => {
+            state.loading = false;
+            state.success = true;
+            state.error = null;
+            state.photo = action.payload;
+            state.photos.unshift(state.photo);
+            state.message = "Foto publicada com sucesso!";
+        }).addCase(publishPhoto.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+            state.photo = {};
+        });
     }
 });
 
