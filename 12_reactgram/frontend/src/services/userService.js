231e35d216148c0c660e441235717e7ac3bd43f4
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 15:45:06 -0300

    Service para resgatar dados do usuário por id

--- 12_reactgram/frontend/src/services/userService.js
+++ 12_reactgram/frontend/src/services/userService.js
@@ -26,5 +26,18 @@ const updateProfile = async (data, token) => {
     }
 }
 
-const userService = { profile, updateProfile };
+// Get user details
+const getUserDetails = async (id) => {
+    const config = requestConfig("GET");
+
+    try {
+        const res = await fetch(api + "/users/" + id, config).then((res) => res.json()).catch((err) => err);
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const userService = { profile, updateProfile, getUserDetails };
 export default userService;
