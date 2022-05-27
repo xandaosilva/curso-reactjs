b3b62d254103333c9ccb0c89f4b049b386944a7c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 20:01:14 -0300

    Service de resgatar foto do usuário

--- 12_reactgram/frontend/src/services/photoService.js
+++ 12_reactgram/frontend/src/services/photoService.js
@@ -12,6 +12,18 @@ const publishPhoto = async (data, token) => {
     }
 }
 
-const photoService = { publishPhoto };
+// get user photos
+const getUserPhotos = async (id) => {
+    const config = requestConfig("GET");
+
+    try {
+        const res = await fetch(api + "/photos/user/" + id, config).then((res) => res.json()).catch((err) => err);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const photoService = { publishPhoto, getUserPhotos };
 
 export default photoService;
