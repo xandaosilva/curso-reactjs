d6fbd04675c9fc4f1221a5c8446a75fdd3d7ad9f
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 13:47:59 -0300

    Exibindo imagem de preview

--- 12_reactgram/frontend/src/pages/EditProfile/EditProfile.js
+++ 12_reactgram/frontend/src/pages/EditProfile/EditProfile.js
@@ -38,6 +38,15 @@ const EditProfile = () => {
         }
     }, [user]);
 
+    const handleFile = (e) => {
+        // Image preview
+        const image = e.target.files[0];
+        setPreviewImage(image);
+
+        // Update image state
+        setProfileImage(image);
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
     }
@@ -47,12 +56,15 @@ const EditProfile = () => {
             <h2>Edite seus dados</h2>
             <p className="subtitle">Adicione uma imagem de perfil e conte mais sobre você.</p>
             {/* Preview da imagem */}
+            {( user.profileImage || previewImage) && (
+                <img className="profile-image" src={ previewImage ? URL.createObjectURL(previewImage) : `${uploads}/users/${user.profileImage}` } alt={user.name} />
+            )}
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""} />
                 <input type="email" placeholder="E-mail" disabled value={email || ""} />
                 <label>
                     <span>Imagem de perfil:</span>
-                    <input type="file" />
+                    <input type="file" onChange={handleFile}/>
                 </label>
                 <label>
                     <span>Bio:</span>
