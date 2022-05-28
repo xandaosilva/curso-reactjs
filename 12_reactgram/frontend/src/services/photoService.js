5bbdafb6033e45a009e0598032b1c4ce7828f42f
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 21:50:35 -0300

    Service de exclusão de fotos

--- 12_reactgram/frontend/src/services/photoService.js
+++ 12_reactgram/frontend/src/services/photoService.js
@@ -24,6 +24,18 @@ const getUserPhotos = async (id, token) => {
     }
 }
 
-const photoService = { publishPhoto, getUserPhotos };
+// Delete a photo
+const deletePhoto = async (id, token) => {
+    const config = requestConfig("DELETE", null, token);
+
+    try {
+        const res = await fetch(api + "/photos/" + id, config).then((res) => res.json()).catch((err) => err);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const photoService = { publishPhoto, getUserPhotos, deletePhoto };
 
 export default photoService;
