cd4579e48e2e0a30b775dbd6ac864e026e9f05c8
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-27 23:45:31 -0300

    Finalizando edição de foto

--- 12_reactgram/frontend/src/pages/Profile/Profile.js
+++ 12_reactgram/frontend/src/pages/Profile/Profile.js
@@ -14,7 +14,7 @@ import { useParams } from "react-router-dom";
 
 // Redux
 import { getUserDetails } from "../../slices/userSlice";
-import { publishPhoto, resetMessage, getUserPhotos, deletePhoto } from "../../slices/photoSlice";
+import { publishPhoto, resetMessage, getUserPhotos, deletePhoto, updatePhoto } from "../../slices/photoSlice";
 
 const Profile = () => {
     const { id } = useParams();
@@ -27,6 +27,10 @@ const Profile = () => {
 
     const[title, setTitle] = useState("");    
     const[image, setImage] = useState("");
+
+    const [editId, setEditId] = useState("");
+    const [editImage, setEditImage] = useState("");
+    const [editTitle, setEditTitle] = useState("");
 
     // New form and edit form refs
     const newPhotoForm = useRef();
@@ -71,6 +75,38 @@ const Profile = () => {
         resetComponentMessage();
     }
 
+    // Show or hide forms
+    const hideOrShowForms = () => {
+        newPhotoForm.current.classList.toggle("hide");
+        editPhotoForm.current.classList.toggle("hide");
+    }
+
+    // Open edit form
+    const handleEdit = (photo) => {
+        if(editPhotoForm.current.classList.contains("hide")){
+            hideOrShowForms();
+        }
+
+        setEditId(photo._id);
+        setEditTitle(photo.title);
+        setEditImage(photo.image);
+    }
+
+    // Cancel edit
+    const handleCancelEdit = () => {
+        hideOrShowForms();
+    }
+
+    // Update a photo
+    const handleUpdate = (e) => {
+        e.preventDefault();
+
+        const photoData = { title: editTitle, id: editId };
+
+        dispatch(updatePhoto(photoData));
+        resetComponentMessage();
+    }
+
     if(loading){
         return (<p>Carregando...</p>);
     }
@@ -103,6 +139,17 @@ const Profile = () => {
                             {loadingPhoto && <input type="submit" value="Aguarde..." disabled />}
                         </form>
                     </div>
+                    <div className="edit-photo hide" ref={editPhotoForm}>
+                        <p>Editando:</p>
+                        {editImage && (
+                            <img src={`${uploads}/photos/${editImage}`} alt={editTitle} />
+                        )}
+                        <form onSubmit={handleUpdate}>    
+                            <input type="text" placeholder="Insira o novo título" onChange={(e) => setEditTitle(e.target.value)} value={editTitle || ""} />
+                            <input type="submit" value="Atualizar" />
+                            <button className="cancel-btn" onClick={handleCancelEdit}>Cancelar edição</button>
+                        </form>
+                    </div>
                     {errorPhoto && <Message msg={errorPhoto} type="error" />}
                     {messagePhoto && <Message msg={messagePhoto} type="success" />}
                 </>
@@ -118,7 +165,7 @@ const Profile = () => {
                                     <Link to={`/photos/${photo._id}`}>
                                         <BsFillEyeFill />
                                     </Link>
-                                    <BsPencilFill />
+                                    <BsPencilFill onClick={() => handleEdit(photo)} />
                                     <BsXLg onClick={() => handleDelete(photo._id)} />
                                 </div>) : 
                                 (<Link to={`/photos/${photo._id}`} className="btn">Ver</Link>)
