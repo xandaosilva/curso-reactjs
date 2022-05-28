016c248fc2e28d0fe2dec578dc6ebd6ab671c0aa
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-28 18:28:57 -0300

    Criando página de foto individual

--- 12_reactgram/frontend/src/App.js
+++ 12_reactgram/frontend/src/App.js
@@ -16,6 +16,7 @@ import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import EditProfile from './pages/EditProfile/EditProfile';
 import Profile from './pages/Profile/Profile';
+import Photo from './pages/Photo/Photo';
 
 function App() {
   const { auth, loading } = useAuth();
@@ -35,6 +36,7 @@ function App() {
             <Route path="/users/:id" element={auth ? <Profile /> : <Navigate to="/login" />} />
             <Route path="/login" element={!auth ? <Login /> : <Navigate to="/" />} />
             <Route path="/register" element={!auth ? <Register /> : <Navigate to="/" />} />
+            <Route path="/photos/:id" element={auth ? <Photo /> : <Navigate to="/login" />} />
           </Routes>
         </div>
         <Footer />
---
+++ 12_reactgram/frontend/src/pages/Photo/Photo.js
@@ -0,0 +1,22 @@
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+// Components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// Redux
+
+const Photo = () => {
+    return (
+        <div>Photo</div>
+    );
+}
+
+export default Photo;
