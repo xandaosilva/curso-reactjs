ba58b4a4ec7cc42dd033bbc6b1946e07a414993a
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-29 07:19:47 -0300

    Primeiro programa com TS

---
+++ 15_apendiceC/index.js
@@ -0,0 +1,7 @@
+var firstName = "Alexandre";
+var anotherName = 22;
+var x = true;
+function greeting(name) {
+    console.log("Ol\u00E1 ".concat(name));
+}
+greeting(firstName);
---
+++ 15_apendiceC/index.ts
@@ -0,0 +1,9 @@
+const firstName = "Alexandre";
+const anotherName = 22;
+const x = true;
+
+function greeting(name: string){
+    console.log(`Olá ${name}`);
+}
+
+greeting(firstName);
