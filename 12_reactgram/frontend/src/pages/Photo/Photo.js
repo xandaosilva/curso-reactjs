d27a5d6dda4b16b36d7fc9932d66bbbefd9251ef
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-28 23:45:31 -0300

    Implementando mensagem nas fotos

--- 12_reactgram/frontend/src/pages/Photo/Photo.js
+++ 12_reactgram/frontend/src/pages/Photo/Photo.js
@@ -12,6 +12,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // Redux
 import { getPhoto, like } from "../../slices/photoSlice";
@@ -20,6 +21,8 @@ const Photo = () => {
     const { id } = useParams();
 
     const dispatch = useDispatch();
+
+    const resetMessage = useResetComponentMessage(dispatch);
 
     const { user } = useSelector((state) => state.auth);
     const { photo, loading, error, message } = useSelector((state) => state.photo);
@@ -35,6 +38,7 @@ const Photo = () => {
     // Like and comments
     const handleLike = () => {
         dispatch(like(photo._id));
+        resetMessage();
     }
 
     if(loading){
@@ -45,6 +49,10 @@ const Photo = () => {
         <div id="photo">
             <PhotoItem photo={photo} />
             <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <div className="message-container">
+                {error && <Message msg={error} type="error" />}
+                {message && <Message msg={message} type="success" />}
+            </div>
         </div>
     );
 }
