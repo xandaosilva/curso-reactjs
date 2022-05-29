13eeaf17d041cf5b200e6d72cf47acde166e92af
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-29 04:14:59 -0300

    Arrow functions

--- 13_apendiceA/assets/js/scripts.js
+++ 13_apendiceA/assets/js/scripts.js
@@ -34,3 +34,44 @@ const name = "Segundo name";
 
 logName();
 console.log(name);
+
+// Arrow function
+const sum = function sum(a, b){
+    return a + b;
+}
+
+const arrowSum = (a, b) => a + b;
+
+const greeting = (name) => {
+    if(name)
+        return `Olá ${name} !`;
+    else
+        return `Olá`;
+}
+
+const testArrow = () => console.log("Teste");
+
+const user = {
+    name: "Theo",
+    sayUserName(){
+        var self= this;
+        setTimeout(function(){
+            console.log(self);
+            console.log(`Username: ${self.name}`);
+        }, 500);
+    },
+    sayUserNameArrow(){
+        setTimeout(() => {
+            console.log(this);
+            console.log(`Username: ${this.name}`);
+        }, 700);
+    }
+}
+
+console.log(sum(5,5));
+console.log(sum(10, 12));
+console.log(greeting());
+console.log(greeting("Alexandre"));
+testArrow();
+user.sayUserName();
+user.sayUserNameArrow();
