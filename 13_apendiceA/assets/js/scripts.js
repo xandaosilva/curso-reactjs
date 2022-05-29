5d2eb0ba025923b86ac1c7292ba6814e669ae34e
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-29 05:24:02 -0300

    Herança

--- 13_apendiceA/assets/js/scripts.js
+++ 13_apendiceA/assets/js/scripts.js
@@ -159,3 +159,22 @@ const shirt = new Product("Camiseta", 20);
 console.log(shirt);
 console.log(shirt.productWithDiscount(10));
 
+// Herança
+class ProductWithAttributes extends Product{
+    constructor(name, price, colors){
+        super(name, price);
+        this.colors = colors;
+    }
+
+    showColors(){
+        console.log("As cores são:");
+        this.colors.forEach((color) => {
+            console.log(color);
+        });
+    }
+}
+
+const newShirt = new ProductWithAttributes("Camisa social", 30.00, ["Azul", "Preto", "Branco"]);
+
+console.log(newShirt);
+newShirt.showColors();
