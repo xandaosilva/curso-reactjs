b1e81ce9f5bbf4f7bb7b24f888d263b4a41b5cf0
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 14:18:25 -0300

    Variáveis no React com TS

--- 16_apendiceD/reactcomts/src/App.tsx
+++ 16_apendiceD/reactcomts/src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
+
+  // Variáveis
+  const name: string = "Alexandre";
+  const age: number = 30;
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking ? (<p>Está trabalhando</p>) : (<p>Não está trabalhando</p>)}
     </div>
   );
 }
