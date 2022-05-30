82b1659e0d6a582b37bf93fee5c77aaad7a5a663
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 16:08:13 -0300

    Context API

--- 16_apendiceD/reactcomts/src/App.tsx
+++ 16_apendiceD/reactcomts/src/App.tsx
@@ -1,3 +1,5 @@
+import { createContext } from "react";
+
 // importação de componentes
 import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
@@ -6,6 +8,11 @@ import State from "./components/State";
 
 // type
 type textOrNull = string | null;
+
+// context
+interface IAppContext { language: string, framework: string, projects: number };
+
+export const AppContext = createContext<IAppContext | null>(null);
 
 function App() {
 
@@ -22,26 +29,31 @@ function App() {
   let mySecondText: textOrNull = null;
   mySecondText = "Olá pessoas";
 
+  // context
+  const contextValue: IAppContext = { language: "JavaScript", framework: "Express", projects: 5 };
+
   return (
-    <div className="App">
-      <h1>TypeScript com React</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking ? (<p>Está trabalhando</p>) : (<p>Não está trabalhando</p>)}
-      <hr />
-      <h3>{userGreeting(name)}</h3>
-      <hr />
-      <FirstComponent />
-      <hr />
-      <SecondComponent name="Segundo" />
-      <hr />
-      <Destructuring title="Curso React" content="Aprendendo React" commentsQty={3} tags={["html", "react", "typeScript"]} category={Category.TS} />
-      <hr />
-      <State />
-      <hr />
-      <p>{myText}</p>
-      <p>{mySecondText}</p>
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>TypeScript com React</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking ? (<p>Está trabalhando</p>) : (<p>Não está trabalhando</p>)}
+        <hr />
+        <h3>{userGreeting(name)}</h3>
+        <hr />
+        <FirstComponent />
+        <hr />
+        <SecondComponent name="Segundo" />
+        <hr />
+        <Destructuring title="Curso React" content="Aprendendo React" commentsQty={3} tags={["html", "react", "typeScript"]} category={Category.TS} />
+        <hr />
+        <State />
+        <hr />
+        <p>{myText}</p>
+        <p>{mySecondText}</p>
+      </div>
+    </AppContext.Provider>
   );
 }
 
