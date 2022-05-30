727e88ca300bea84f294a9aa7c8576cd7ed85ca5
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 14:34:11 -0300

    Criando componentes em React com TS

---
+++ 16_apendiceD/reactcomts/src/components/FirstComponent.tsx
@@ -0,0 +1,10 @@
+// componente
+import React, { ReactElement } from 'react'
+
+const FirstComponent = (): ReactElement => {
+    return (
+        <div>FirstComponent</div>
+    );
+}
+
+export default FirstComponent;
