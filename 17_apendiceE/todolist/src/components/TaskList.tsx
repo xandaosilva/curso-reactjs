36087732d33f3de50ab8ab6b0c24bfbeb231465c
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 18:36:24 -0300

    Criando formulário

---
+++ 17_apendiceE/todolist/src/components/TaskList.tsx
@@ -0,0 +1,13 @@
+import styles from "./TaskList.module.css";
+
+type Props = {}
+
+const TaskList = (props: Props) => {
+    return (
+        <div>
+            <p>Lista de tarefas</p>
+        </div>
+    );
+}
+
+export default TaskList;
