f6cd73be8ca8604741b855af062100249e0f663e
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 14:24:48 -0300

    Funções em componentes em TS

--- 16_apendiceD/reactcomts/src/App.tsx
+++ 16_apendiceD/reactcomts/src/App.tsx
@@ -5,12 +5,17 @@ function App() {
   const age: number = 30;
   const isWorking: boolean = true;
 
+  // funções
+  const userGreeting = (name: string): string => `Olá, ${name} !`;
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking ? (<p>Está trabalhando</p>) : (<p>Não está trabalhando</p>)}
+      <hr />
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
