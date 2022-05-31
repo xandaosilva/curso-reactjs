f28d94e374279fe71e5e080536fbe71e80fc1e84
Alexandre Rogério <xandaosilva93@hotmail.com>
2022-05-30 21:00:41 -0300

    HTML dos itens da lista

--- 17_apendiceE/todolist/src/components/TaskList.tsx
+++ 17_apendiceE/todolist/src/components/TaskList.tsx
@@ -10,7 +10,14 @@ const TaskList = ({taskList}: Props) => {
             {taskList.length > 0 ? 
                 (taskList.map((task) => (
                     <div key={task.id}>
-                        <p>{task.title}</p>
+                        <div>
+                            <h4>{task.title}</h4>
+                            <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                        <div>
+                            <i className="bi bi-pencil"></i>
+                            <i className="bi bi-trash"></i>
+                        </div>
                     </div>
                 ))) : 
                 (<p>Não há tarefas cadastradas.</p>)
